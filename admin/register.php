8f473fdeed30687fc7c5a13638965ba7a9bcacfd
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-16 17:44:04 +0200

    register

---
+++ admin/register.php
@@ -0,0 +1,57 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body style="padding-left: 0;">
+
+<section class="form-container">
+    <form class="register" action="" method="post" enctype="multipart/form-data">
+        <h3>register new</h3>
+      <div class="flex">
+            <div class="col">
+                <p>your name <span>*</span></p>
+                <input type="text" name="name" placeholder="eneter your name" maxlength="50" required class="box">
+                <p>your profession <span>*</span></p>
+                <select name="profession" class="box" required>
+
+
+                </select>
+                <p>your email <span>*</span></p>
+                <input type="email" name="email" placeholder="enter your email" maxlength="20" required class="box">
+            </div>
+            <div class="col">
+                <p>your password <span>*</span></p>
+                <input type="password" name="pass" placeholder="enter your password" maxlength="20" required class="box">
+                <p>confirm password <span>*</span></p>
+                <input type="password" name="cpass" placeholder="confirm your password" maxlength="20" required class="box">
+                <p>select pic <span>*</span></p>
+                <input type="file" name="image" accept="image/*" required class="box">
+            </div>
+      </div>
+      <p class="link">already have an account? <a href="login.php">login now</a></p>
+      <input type="submit" name="submit" value="register now" class="btn">
+   </form>
+
+</section>
+
+
+
+
+</section>
+
+
+</body>
+</html>
