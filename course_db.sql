444884912153bcad714420cc8d69be38a69781f7
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-16 17:38:23 +0200

    course db

--- course_db.sql
+++ course_db.sql
@@ -85,3 +85,17 @@ CREATE TABLE `playlist` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
+--
+-- Table structure for table `tutors`
+--
+
+CREATE TABLE `tutors` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `profession` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
