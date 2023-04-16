a2fa497ff0e35166bf63c9b3b35090e8f54d8901
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-16 17:38:58 +0200

    course db

--- course_db.sql
+++ course_db.sql
@@ -99,3 +99,15 @@ CREATE TABLE `tutors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
