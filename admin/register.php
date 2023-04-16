ae9adf775c535645f28fa0bdd75748104e301dd3
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-16 17:46:51 +0200

    register

--- admin/register.php
+++ admin/register.php
@@ -1,4 +1,48 @@
+<?php
 
+include '../components/connect.php';
+
+if(isset($_POST['submit'])){
+
+   $id = unique_id();
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $profession = $_POST['profession'];
+   $profession = filter_var($profession, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+   $cpass = sha1($_POST['cpass']);
+   $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+
+   $image = $_FILES['image']['name'];
+   $image = filter_var($image, FILTER_SANITIZE_STRING);
+   $ext = pathinfo($image, PATHINFO_EXTENSION);
+   $rename = unique_id().'.'.$ext;
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folder = '../uploaded_files/'.$rename;
+
+   $select_tutor = $conn->prepare("SELECT * FROM `tutors` WHERE email = ?");
+   $select_tutor->execute([$email]);
+   
+   if($select_tutor->rowCount() > 0){
+      $message[] = 'email already taken!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm passowrd not matched!';
+      }else{
+         $insert_tutor = $conn->prepare("INSERT INTO `tutors`(id, name, profession, email, password, image) VALUES(?,?,?,?,?,?)");
+         $insert_tutor->execute([$id, $name, $profession, $email, $cpass, $rename]);
+         move_uploaded_file($image_tmp_name, $image_folder);
+         $message[] = 'new tutor registered! please login now';
+      }
+   }
+
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,6 +60,19 @@
 
 </head>
 <body style="padding-left: 0;">
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message form">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
 
 <section class="form-container">
     <form class="register" action="" method="post" enctype="multipart/form-data">
