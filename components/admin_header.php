8b1505b40a418d2fb940d75611b5e61b5db03e6a
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-17 11:52:41 +0200

    admin header

---
+++ components/admin_header.php
@@ -0,0 +1,29 @@
+
+
+
+<header class="header">
+
+   <section class="flex">
+
+      <a href="dashboard.php" class="logo">Admin.</a>
+
+      <form action="search_page.php" method="post" class="search-form">
+         <input type="text" name="search" placeholder="search here..." required maxlength="100">
+         <button type="submit" class="fas fa-search" name="search_btn"></button>
+      </form>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="toggle-btn" class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+
+      </div>
+
+
+    </section>
+
+</header>
