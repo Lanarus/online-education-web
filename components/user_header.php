6d225120c35f199a1ce99fa135ee2d728cbba789
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-20 21:49:23 +0200

    user header

---
+++ components/user_header.php
@@ -0,0 +1,27 @@
+
+
+
+
+<header class="header">
+
+   <section class="flex">
+
+      <a href="home.php" class="logo">Educa.</a>
+
+      <form action="search_course.php" method="post" class="search-form">
+         <input type="text" name="search_course" placeholder="search courses..." required maxlength="100">
+         <button type="submit" class="fas fa-search" name="search_course_btn"></button>
+      </form>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="toggle-btn" class="fas fa-sun"></div>
+      </div>
+
+      </section>
+
+</header>
+
+
