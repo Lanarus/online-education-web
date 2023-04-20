4824208a4a6dcb3512b94deab4a61fa762746d8b
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-20 21:54:44 +0200

    user header profile

--- components/user_header.php
+++ components/user_header.php
@@ -19,8 +19,35 @@
          <div id="user-btn" class="fas fa-user"></div>
          <div id="toggle-btn" class="fas fa-sun"></div>
       </div>
-
-      </section>
+      <div class="profile">
+            <?php
+                $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+                $select_profile->execute([$user_id]);
+                if($select_profile->rowCount() > 0){
+                $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+            ?>
+            <img src="uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+            <h3><?= $fetch_profile['name']; ?></h3>
+            <span>student</span>
+            <a href="profile.php" class="btn">view profile</a>
+            <div class="flex-btn">
+                <a href="login.php" class="option-btn">login</a>
+                <a href="register.php" class="option-btn">register</a>
+            </div>
+            <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+            <?php
+                }else{
+            ?>
+            <h3>please login or register</h3>
+            <div class="flex-btn">
+                <a href="login.php" class="option-btn">login</a>
+                <a href="register.php" class="option-btn">register</a>
+            </div>
+            <?php
+                }
+            ?>
+        </div>
+    </section>
 
 </header>
 
