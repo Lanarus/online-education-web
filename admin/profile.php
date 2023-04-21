6a07ed6a4351b2272a22e8e4e6e9376cb32a89b1
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-21 10:37:51 +0200

    admin profile

---
+++ admin/profile.php
@@ -0,0 +1,38 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Profile</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="tutor-profile" style="min-height: calc(100vh - 19rem);"> 
+
+   <h1 class="heading">profile details</h1>
+
+   <div class="details">
+
+   </div>
+
+</section>
+
+<?php include '../components/footer.php'; ?>
+
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
