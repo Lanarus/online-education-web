9c81e6b9011f86ece6389605d57adc707604448e
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-21 11:45:52 +0200

    admin playlists

--- admin/playlists.php
+++ admin/playlists.php
@@ -1,4 +1,40 @@
+<?php
 
+include '../components/connect.php';
+
+if(isset($_COOKIE['tutor_id'])){
+   $tutor_id = $_COOKIE['tutor_id'];
+}else{
+   $tutor_id = '';
+   header('location:login.php');
+}
+
+if(isset($_POST['delete'])){
+   $delete_id = $_POST['playlist_id'];
+   $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
+
+   $verify_playlist = $conn->prepare("SELECT * FROM `playlist` WHERE id = ? AND tutor_id = ? LIMIT 1");
+   $verify_playlist->execute([$delete_id, $tutor_id]);
+
+   if($verify_playlist->rowCount() > 0){
+
+   
+
+   $delete_playlist_thumb = $conn->prepare("SELECT * FROM `playlist` WHERE id = ? LIMIT 1");
+   $delete_playlist_thumb->execute([$delete_id]);
+   $fetch_thumb = $delete_playlist_thumb->fetch(PDO::FETCH_ASSOC);
+   unlink('../uploaded_files/'.$fetch_thumb['thumb']);
+   $delete_bookmark = $conn->prepare("DELETE FROM `bookmark` WHERE playlist_id = ?");
+   $delete_bookmark->execute([$delete_id]);
+   $delete_playlist = $conn->prepare("DELETE FROM `playlist` WHERE id = ?");
+   $delete_playlist->execute([$delete_id]);
+   $message[] = 'playlist deleted!';
+   }else{
+      $message[] = 'playlist already deleted!';
+   }
+}
+
+?>
 
 
 <!DOCTYPE html>
@@ -25,7 +61,16 @@
             <h3 class="title" style="margin-bottom: .5rem;">create new playlist</h3>
             <a href="add_playlist.php" class="btn">add playlist</a>
         </div>
-
+        <?php
+            $select_playlist = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ? ORDER BY date DESC");
+            $select_playlist->execute([$tutor_id]);
+            if($select_playlist->rowCount() > 0){
+            while($fetch_playlist = $select_playlist->fetch(PDO::FETCH_ASSOC)){
+                $playlist_id = $fetch_playlist['id'];
+                $count_videos = $conn->prepare("SELECT * FROM `content` WHERE playlist_id = ?");
+                $count_videos->execute([$playlist_id]);
+                $total_videos = $count_videos->rowCount();
+        ?>
         <div class="box">
             <div class="flex">
                 <div><i class="fas fa-circle-dot" style="<?php if($fetch_playlist['status'] == 'active'){echo 'color:limegreen'; }else{echo 'color:red';} ?>"></i><span style="<?php if($fetch_playlist['status'] == 'active'){echo 'color:limegreen'; }else{echo 'color:red';} ?>"><?= $fetch_playlist['status']; ?></span></div>
@@ -46,11 +91,11 @@
       
         </div>
         <?php
-         } 
-      }else{
-         echo '<p class="empty">no playlist added yet!</p>';
-      }
-      ?>
+            } 
+        }else{
+            echo '<p class="empty">no playlist added yet!</p>';
+        }
+        ?>
     </div>
 </section>
 
