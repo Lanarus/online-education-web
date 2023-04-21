7c0fa5d4f9156e26e5326d142bea583c21a0ee9c
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-21 11:35:47 +0200

    admin playlists

---
+++ admin/playlists.php
@@ -0,0 +1,34 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playlists</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="../css/admin_style1.css">
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="playlists">
+
+    <h1 class="heading">added playlists</h1>
+
+    <div class="box-container">
+        <div class="box" style="text-align: center;">
+            <h3 class="title" style="margin-bottom: .5rem;">create new playlist</h3>
+            <a href="add_playlist.php" class="btn">add playlist</a>
+        </div>
+
+    </div>
+</section>
+
+<?php include '../components/footer.php'; ?>
+</body>
+</html>
