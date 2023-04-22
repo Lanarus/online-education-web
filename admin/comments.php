3a0648960401bef7f136258ba3a8622165474f2b
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-22 07:30:49 +0200

    admin coments

---
+++ admin/comments.php
@@ -0,0 +1,52 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style1.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+   
+<section class="comments">
+
+   <h1 class="heading">user comments</h1>
+
+   
+   <div class="show-comments">
+
+   </div>
+   
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include '../components/footer.php'; ?>
+
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
