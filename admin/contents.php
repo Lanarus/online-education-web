cfa4d786ce86a276789a9b9f27b0c0c580c60430
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-22 07:57:29 +0200

    admin contents

---
+++ admin/contents.php
@@ -0,0 +1,43 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style1.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="contents">
+
+   <h1 class="heading">your contents</h1>
+
+   <div class="box-container">
+
+        <div class="box" style="text-align: center;">
+            <h3 class="title" style="margin-bottom: .5rem;">create new content</h3>
+            <a href="add_content.php" class="btn">add content</a>
+        </div>
+
+   </div>
+
+</section>
+
+<?php include '../components/footer.php'; ?>
+
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
