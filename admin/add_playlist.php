c72a1f3e1020c42cdd8de2d26c60f300089f4f0c
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-24 17:50:42 +0200

    admin add playlist

--- admin/add_playlist.php
+++ admin/add_playlist.php
@@ -13,7 +13,7 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/admin_style.css">
+   <link rel="stylesheet" href="../css/admin_style1.css">
 
 </head>
 <body>
@@ -24,7 +24,21 @@
 
    <h1 class="heading">create playlist</h1>
 
-
+   <form action="" method="post" enctype="multipart/form-data">
+      <p>playlist status <span>*</span></p>
+      <select name="status" class="box" required>
+         <option value="" selected disabled>-- select status</option>
+         <option value="active">active</option>
+         <option value="deactive">deactive</option>
+      </select>
+      <p>playlist title <span>*</span></p>
+      <input type="text" name="title" maxlength="100" required placeholder="enter playlist title" class="box">
+      <p>playlist description <span>*</span></p>
+      <textarea name="description" class="box" required placeholder="write description" maxlength="1000" cols="30" rows="10"></textarea>
+      <p>playlist thumbnail <span>*</span></p>
+      <input type="file" name="image" accept="image/*" required class="box">
+      <input type="submit" value="create playlist" name="submit" class="btn">
+   </form>
 
 
 
