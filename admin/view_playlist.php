1d30e6d3e310b7d2468c5b697c0ee8f57d66a8fe
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-27 23:11:58 +0200

    admin view playlist

--- admin/view_playlist.php
+++ admin/view_playlist.php
@@ -41,6 +41,11 @@ if(isset($_POST['delete_video'])){
         $fetch_thumb = $delete_video_thumb->fetch(PDO::FETCH_ASSOC);
         unlink('../uploaded_files/'.$fetch_thumb['thumb']);
         $delete_video = $conn->prepare("SELECT * FROM `content` WHERE id = ? LIMIT 1");
+        $delete_video->execute([$delete_id]);
+        $fetch_video = $delete_video->fetch(PDO::FETCH_ASSOC);
+        unlink('../uploaded_files/'.$fetch_video['video']);
+        $delete_likes = $conn->prepare("DELETE FROM `likes` WHERE content_id = ?");
+        $delete_likes->execute([$delete_id]);
 
 }
 
