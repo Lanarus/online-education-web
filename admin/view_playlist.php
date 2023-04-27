31f533ea5626e7abd5928a4249f0f89e155fa708
Lanarus <ruslanasabitova0321@gmail.com>
2023-04-27 22:57:34 +0200

    admin view playlist

---
+++ admin/view_playlist.php
@@ -0,0 +1,51 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Playlist Details</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+   
+<section class="playlist-details">
+
+   <h1 class="heading">playlist details</h1>
+
+
+</section>
+
+<section class="contents">
+
+   <h1 class="heading">playlist videos</h1>
+
+   <div class="box-container">   
+
+
+
+   </div>
+
+</section>
+
+
+
+
+
+<?php include '../components/footer.php'; ?>
+
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
