0495d6683f2c1ba8cc8e51c92444aaa6361b387e
Lanarus <ruslanasabitova0321@gmail.com>
2023-05-09 23:39:17 +0200

    admin search page

---
+++ admin/search_page.php
@@ -0,0 +1,51 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+   
+<section class="contents">
+
+   <h1 class="heading">contents</h1>
+
+   <div class="box-container">
+
+
+
+   </div>
+
+</section>
+
+<section class="playlists">
+
+   <h1 class="heading">playlists</h1>
+
+   <div class="box-container">
+
+   </div>
+
+</section>
+
+<?php include '../components/footer.php'; ?>
+
+
+
+</body>
+</html>
