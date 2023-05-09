b4c1c2cb0db1a9604d29fd016179b69f53c2e0be
Lanarus <ruslanasabitova0321@gmail.com>
2023-05-09 23:41:14 +0200

    admin search page

--- admin/search_page.php
+++ admin/search_page.php
@@ -27,7 +27,20 @@
 
    <div class="box-container">
 
-
+        <div class="box">
+            <div class="flex">
+                <div><i class="fas fa-dot-circle" style="<?php if($fecth_videos['status'] == 'active'){echo 'color:limegreen'; }else{echo 'color:red';} ?>"></i><span style="<?php if($fecth_videos['status'] == 'active'){echo 'color:limegreen'; }else{echo 'color:red';} ?>"><?= $fecth_videos['status']; ?></span></div>
+                <div><i class="fas fa-calendar"></i><span><?= $fecth_videos['date']; ?></span></div>
+            </div>
+            <img src="../uploaded_files/<?= $fecth_videos['thumb']; ?>" class="thumb" alt="">
+            <h3 class="title"><?= $fecth_videos['title']; ?></h3>
+            <form action="" method="post" class="flex-btn">
+                <input type="hidden" name="video_id" value="<?= $video_id; ?>">
+                <a href="update_content.php?get_id=<?= $video_id; ?>" class="option-btn">update</a>
+                <input type="submit" value="delete" class="delete-btn" onclick="return confirm('delete this video?');" name="delete_video">
+            </form>
+            <a href="view_content.php?get_id=<?= $video_id; ?>" class="btn">view content</a>
+      </div>
 
    </div>
 
