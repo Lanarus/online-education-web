e3d2c7b8e8f98faa519e21295e45af4849ee43af
Lanarus <ruslanasabitova0321@gmail.com>
2023-05-10 22:53:29 +0200

    admin search page

--- admin/search_page.php
+++ admin/search_page.php
@@ -44,7 +44,21 @@ if(isset($_POST['delete_playlist'])){
  
     if($verify_playlist->rowCount() > 0){
 
+
+    $delete_playlist_thumb = $conn->prepare("SELECT * FROM `playlist` WHERE id = ? LIMIT 1");
+    $delete_playlist_thumb->execute([$delete_id]);
+    $fetch_thumb = $delete_playlist_thumb->fetch(PDO::FETCH_ASSOC);
+    unlink('../uploaded_files/'.$fetch_thumb['thumb']);
+    $delete_bookmark = $conn->prepare("DELETE FROM `bookmark` WHERE playlist_id = ?");
+    $delete_bookmark->execute([$delete_id]);
+    $delete_playlist = $conn->prepare("DELETE FROM `playlist` WHERE id = ?");
+    $delete_playlist->execute([$delete_id]);
+    $message[] = 'playlist deleted!';
+    }else{
+      $message[] = 'playlist already deleted!';
     }
+
+ }
 
 
 ?>
